efe2aeedf09d8381fb936e699139f714ec31e01d
Raj Bedi <raj.j.bedi@gmail.com>
2022-11-08 16:09:34 -0500

    added css and basic setup for code igniter

---
+++ application/views/templates/header.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>ciBlog</title>
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    </head>
+    <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary ">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="/">ciBlog</a>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarColor01">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo base_url(); ?>">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo base_url(); ?>about">About</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+        
