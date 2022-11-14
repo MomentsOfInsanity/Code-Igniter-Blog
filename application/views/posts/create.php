0304c592315c62561f8875e59616fcd25c361210
Raj Bedi <raj.j.bedi@gmail.com>
2022-11-14 14:03:44 -0500

    added form

---
+++ application/views/posts/create.php
@@ -0,0 +1,14 @@
+<h2><?= $title; ?></h2>
+<form>
+  <div class="form-group">
+    <label>Title</label>
+    <input type="text" class="form-control" name="title" placeholder="Add Title">
+  </div>
+  <br>
+  <div class="form-group">
+    <label>Body</label>
+    <textarea class="form-control" name="body" placeholder="Add Body"></textarea>
+  </div>
+  <br>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
