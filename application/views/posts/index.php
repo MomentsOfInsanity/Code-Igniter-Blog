041d30b505b2314412c2bd0ec211567a9a27f83f
Raj Bedi <raj.j.bedi@gmail.com>
2022-11-14 13:36:31 -0500

    added link to post page

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -4,4 +4,6 @@
     <small class="post-date">Posted on: <?php echo $post['created_at']; ?></small>
     <br>
     <?php echo $post['body']; ?>
+    <br><br>
+    <p><a class="btn btn-primary" href="<?php echo site_url('/posts/'.$post['slug']); ?>">Read More</a></p>
 <?php endforeach; ?>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,14 +1,14 @@
 <html>
     <head>
         <title>ciBlog</title>
-        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">>
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
     </head>
     <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary ">
         <div class="container-fluid">
             <div class="navbar-header">
-                <a class="navbar-brand" href="/">ciBlog</a>
+                <a class="navbar-brand" href="<?php echo base_url(); ?>">ciBlog</a>
             </div>
             <div class="collapse navbar-collapse" id="navbarColor01">
                 <ul class="navbar-nav me-auto">
@@ -17,6 +17,9 @@
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="<?php echo base_url(); ?>about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo base_url(); ?>posts">Blog</a>
                     </li>
                 </ul>
             </div>
